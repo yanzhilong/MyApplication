35268bd32549f4f23ca631b428c42570dddeecd4
yanzl <gfdonx@163.com>
2016-10-13 18:01:06 +0800

    增加测试增加文件类

--- app/src/androidTest/java/com/englishlearn/myapplication/data/TractateTest.java
+++ app/src/androidTest/java/com/englishlearn/myapplication/data/TractateTest.java
@@ -23,6 +23,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import rx.observers.TestSubscriber;
 
@@ -57,6 +59,16 @@ public class TractateTest {
     }
 
 
+    /**
+     * 添加双语文章
+     * 第一行：标题
+     * 第二行：备注
+     * #开头是注释
+     * 每一行一句英文
+     * !!!后面是翻译内容，不包括标题
+     * 每一行一句中文
+     * @throws IOException
+     */
     @Test
     public void addTractate() throws IOException {
 
@@ -135,5 +147,140 @@ public class TractateTest {
             Tractate tractate = list.get(0);
             Log.d(TAG,"testTractate_add_result:" + tractate.toString());
         }
+    }
+
+    /**
+     * 添加文章，以标点符号间隔每一句
+     * @throws IOException
+     */
+    @Test
+    public void addTractateByPunctuation() throws IOException {
+
+        InputStream inputStream = context.getResources().openRawResource(R.raw.myfather);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+
+        List<String> english_paragraph = new ArrayList<>();//英文段落数
+        List<String> chinese_paragraph = new ArrayList<>();//中文段落数
+
+        boolean isTranslation = false;//是否是译文
+
+        String title = bufferedReader.readLine();
+        String remark = bufferedReader.readLine();
+
+        while ((line = bufferedReader.readLine()) != null){
+
+            Log.d(TAG,line);
+            if(!line.startsWith("#") && !line.equals("")){
+
+                if(!isTranslation && !line.startsWith("!!!")){
+                    Log.d(TAG,"英文:"+line);
+                    english_paragraph.add(line);
+
+                }else if(line.startsWith("!!!")){
+                    //是译文
+                    isTranslation = true;
+                }else{
+                    Log.d(TAG,"中文:"+line);
+                    chinese_paragraph.add(line);
+
+                }
+            }
+        }
+        bufferedReader.close();
+        inputStream.close();
+        Log.d(TAG,"英文段落数：" + english_paragraph.size() + "" + chinese_paragraph.size());
+        Log.d(TAG,"中文段落数：" + chinese_paragraph.size());
+        if(english_paragraph.size() != chinese_paragraph.size()){
+            Log.d(TAG,"英文和译文行数不同");
+            return;
+        }
+
+        //判断每一个段落的标点符号数量
+        for(int i = 0; i < english_paragraph.size(); i++){
+
+            String englishParagraph = english_paragraph.get(i);
+            String chinestParagraph = chinese_paragraph.get(i);
+
+            String regEx = "[\\u4e00-\\u9fa5，。！、……a-zA-Z\\d]+";
+            Pattern p = Pattern.compile(regEx,Pattern.CASE_INSENSITIVE);
+            Matcher m = p.matcher(englishParagraph);
+            Matcher m1 = p.matcher(chinestParagraph);
+            boolean rs = m.find();
+            boolean rs1 = m1.find();
+            Log.d(TAG,rs1 ? "中文合法" : "中文不合法");
+            Log.d(TAG,rs ? "英文合法" : "英文不合法");
+            Log.d(TAG,rs ? "中文合法" : "英文不合法");
+            Log.d(TAG,rs ? "英文合法" : "英文不合法");
+            /*int count = 0;
+            while(m.find()){
+                count ++;
+            }
+            System.out.println("ABC的个数 : " + count);*/
+
+        }
+
+        //判断每一个段落的标点符号数量
+        for(int i = 0; i < english_paragraph.size(); i++){
+
+            String englishParagraph = english_paragraph.get(i);
+            String chinestParagraph = chinese_paragraph.get(i);
+
+            String english = "[,.?;!]";//英文标点
+            String chinese = "[，。？；！、]";//中文标点
+            Pattern p = Pattern.compile(english,Pattern.CASE_INSENSITIVE);
+            Pattern p1 = Pattern.compile(chinese,Pattern.CASE_INSENSITIVE);
+            Matcher m = p.matcher(englishParagraph);
+            Matcher m1 = p1.matcher(chinestParagraph);
+            int count = 0;
+            int count1 = 0;
+            while(m.find()){
+                count ++;
+            }
+            while(m1.find()){
+                count1 ++;
+            }
+            Log.d(TAG,"英文第"+i+"段的标点个数 : " + count + "chinese:" + count1);
+            Log.d(TAG,"中文第"+i+"段的标点个数 : " + count1);
+        }
+
+
+        /*StringBuffer content = new StringBuffer();
+        StringBuffer translation = new StringBuffer();
+
+        for(String c:contentlist){
+            content.append(c);
+            content.append("\r\n");
+        }
+
+        for(String t:translationlist){
+            translation.append(t);
+            translation.append("\r\n");
+        }
+        //添加　
+        Log.d(TAG,"testTractate_add");
+        Tractate addtractate = new Tractate();
+        addtractate.setTractatetypeId("880d538e1d");
+        addtractate.setTitle(title);
+        addtractate.setRemark(remark);
+        addtractate.setUserId(Constant.userId0703);
+        addtractate.setContent(content.toString());
+        addtractate.setTranslation(translation.toString());
+
+        TestSubscriber<Tractate> testSubscriber_add = new TestSubscriber<>();
+        mBmobRemoteData.addTractate(addtractate).toBlocking().subscribe(testSubscriber_add);
+        List<Throwable> throwables = testSubscriber_add.getOnErrorEvents();
+        if(throwables.size() > 0){
+            Log.d(TAG,throwables.get(0).getMessage());
+        }
+        testSubscriber_add.assertNoErrors();
+        List<Tractate> list = testSubscriber_add.getOnNextEvents();
+        Assert.assertNotNull(list);
+        if(list != null || list.size() > 0){
+            Tractate tractate = list.get(0);
+            Log.d(TAG,"testTractate_add_result:" + tractate.toString());
+        }*/
     }
 }
